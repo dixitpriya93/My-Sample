6c5114bbe57d45a77821beb579d232435654dd42
Priya Dixit <dixitpriya93@gmail.com>
2017-06-03 17:26:17 -0500

    loginform

---
+++ login.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int a, b, c;
+
+	printf("Enter your numbers \n");
+	scanf("%d, %d", &a, &b);
+	c = a + b;
+	printf("sum is %d \n", c);
+
+	return 0;
+}
